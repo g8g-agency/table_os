1e57d18f5d19cacf3db87f80ccdab3113cc26cf9
Adarsh Shandilya <yashtiwari075@gmail.com>
2026-07-03 17:02:01 +0530

    fix: resolve TS object possibly undefined error in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ const server = app.listen(PORT, '0.0.0.0', () => {
   const results = Object.create(null);
 
   for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
+    for (const net of nets[name] || []) {
       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
       if (net.family === 'IPv4' && !net.internal) {
         if (!results[name]) {
